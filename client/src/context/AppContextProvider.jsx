ca1ab843e75a5d770f293397d4bcdf410eaa7b22
EpicVN <ledangkhoahi3@gmail.com>
2025-04-16 00:52:21 +0700

    feat(client): update AppContextProvider and complete CourseDetails page UI
    - Add new functionality (calculateChapterTime, calculateCourseDuration, calculateNoOfLectures) in AppContextProvider
    - Complete CourseDetails page UI

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -2,7 +2,7 @@ import { use, useEffect, useState } from "react";
 import { AppContext } from "./AppContext";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
-
+import humanizeDuration from "humanize-duration";
 export const AppContextProvider = (props) => {
   const currency = import.meta.env.VITE_CURRENCY;
   const navigate = useNavigate();
@@ -29,6 +29,38 @@ export const AppContextProvider = (props) => {
     return totalRating / course.courseRatings.length;
   };
 
+  // Calculate course chapter time
+  const calculateChapterTime = (chapter) => {
+    let totalTime = 0;
+    chapter.chapterContent.map((lecture) => (totalTime += lecture.lectureDuration));
+
+    return humanizeDuration(totalTime * 60 * 1000, { units: ["h", "m"] });
+  };
+
+  // Calculate course duration
+  const calculateCourseDuration = (course) => {
+    let totalTime = 0;
+    course.courseContent.map((chapter) =>
+      chapter.chapterContent.map(
+        (lecture) => (totalTime += lecture.lectureDuration)
+      )
+    );
+
+    return humanizeDuration(totalTime * 60 * 1000, { units: ["h", "m"] });
+  };
+
+  // Calculate to No of lectures in the course
+  const calculateNoOfLectures = (course) => {
+    let totalLectures = 0;
+    course.courseContent.map((chapter) => {
+      if (Array.isArray(chapter.chapterContent)) {
+        totalLectures += chapter.chapterContent.length;
+      }
+    });
+
+    return totalLectures;
+  };
+
   useEffect(() => {
     fetchAllCourses();
   }, []);
@@ -40,6 +72,9 @@ export const AppContextProvider = (props) => {
     calculateRating,
     isEducator,
     setIsEducator,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateNoOfLectures,
   };
 
   return (
