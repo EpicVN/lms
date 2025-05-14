6628eb923093373fe734a5af12a53725706ff432
EpicVN <ledangkhoahi3@gmail.com>
2025-05-15 00:05:09 +0700

    feat(server): implement addCourse controller for educator with Cloudinary upload

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,4 +1,6 @@
-import { clerkClient } from "@clerk/express"
+import { clerkClient } from "@clerk/express";
+import Course from "../models/Course.js";
+import { v2 as cloudinary } from "cloudinary";
 
 export const updateRoleToEducator = async (req, res) => {
   try {
@@ -20,5 +22,51 @@ export const updateRoleToEducator = async (req, res) => {
       message: error.message,
     });
     console.log(error);
+  }
+};
+
+// Add new course
+export const addCourse = async (req, res) => {
+  try {
+    const { courseData } = req.body;
+    const imageFile = req.file;
+    const educatorId = req.auth.userId;
+
+    if (!courseData) {
+      return res.status(400).json({
+        success: false,
+        message: "Course data is missing",
+      });
+    }
+
+    if (!imageFile) {
+      return res.status(400).json({
+        success: false,
+        message: "Thumbnail not attached",
+      });
+    }
+
+    const parsedCourseData = await JSON.parse(courseData);
+
+    parsedCourseData.educator = educatorId;
+
+    const newCourse = await Course.create(parsedCourseData);
+
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path);
+
+    newCourse.courseThumbnail = imageUpload.secure_url;
+
+    await newCourse.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "Course Added",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
   }
 };
