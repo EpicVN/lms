f1590a891581c3781c4081fabb5c2d3836beda92
EpicVN <ledangkhoahi3@gmail.com>
2025-06-26 00:38:41 +0700

    feat(server): add get all course and get course by id api

---
+++ server/controllers/courseController.js
@@ -0,0 +1,60 @@
+// get all courses
+
+import Course from "../models/Course.js";
+
+export const getAllCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({ isPublished: true })
+      .select(["-courseContent", "-enrolledStudents"])
+      .populate({ path: "educator" });
+
+    return res.status(200).json({
+      success: true,
+      courses: courses,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// get course by id
+export const getCourseId = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const courseData = await Course.findById(id).populate({
+      path: "educator",
+    });
+
+    if (!courseData) {
+      return res.status(404).json({
+        success: false,
+        message: "Course not found",
+      });
+    }
+
+    // Remove lecture URLs for non-preview lectures
+    courseData.courseContent.forEach((chapter) => {
+      chapter.chapterContent.forEach((lecture) => {
+        if (!lecture.isPreviewFree) {
+          lecture.lectureUrl = "";
+        }
+      });
+    });
+
+    return res.status(200).json({
+      success: true,
+      courseData,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
