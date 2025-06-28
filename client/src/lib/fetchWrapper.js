bd3dae63109d7df70166523a1a99b387c6317c4c
EpicVN <ledangkhoahi3@gmail.com>
2025-06-28 22:37:37 +0700

    feat(client): add custom fetch wrapper

---
+++ client/src/lib/fetchWrapper.js
@@ -0,0 +1,54 @@
+// lib/fetchWrapper.js
+
+export async function fetchWrapper(
+  endpoint,
+  { token, body, ...customConfig } = {}
+) {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  const config = {
+    method: body ? "POST" : "GET",
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...(customConfig.headers || {}),
+    },
+  };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  try {
+    const res = await fetch(endpoint, config);
+    const contentType = res.headers.get("content-type");
+
+    const responseData = contentType?.includes("application/json")
+      ? await res.json()
+      : await res.text();
+
+    // Kiểm tra nếu response không thành công
+    if (!res.ok) {
+      const error = new Error(responseData?.message || "Something went wrong");
+      error.statusCode = res.status;
+      error.data = responseData;
+      throw error;
+    }
+
+    return {
+      success: responseData.success,
+      statusCode: res.status,
+      message: responseData?.message || "",
+      data: responseData,
+    };
+  } catch (error) {
+    console.error("Fetch error:", error);
+    throw error;
+  }
+}
